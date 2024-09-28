22f00b81238a213ed84fe341eb1188e763c3a5d7
justk <justkukaro@naver.com>
2024-09-28 21:03:35 +0900

    feat: 1차 완료

--- chapter4/script.js
+++ chapter4/script.js
@@ -1,5 +1,11 @@
 const resultElement = document.querySelector('.display-body > span');
 
+
+let firstOperand = null;
+let operator = null;
+let secondOperand = null;
+let reset = false;
+
 document.querySelectorAll('.button').forEach((btn) => {
     btn.addEventListener('click', btnClick)
 })
@@ -8,19 +14,26 @@ function btnClick(event) {
     // event.target은 유사 배열 객체라서 includes가 안되니 배열로 바꿔준다
     if (Array.from(event.target.classList).includes('number')) {
         btnClickNumber(event.target.textContent);
+        return;
     }
     if (Array.from(event.target.classList).includes('dot')) {
         btnClickDot();
+        return;
     }
     if (Array.from(event.target.classList).includes('function')) {
         btnClickFunction(event.target.textContent);
+        return;
     }
-    console.log(event.target.textContent);
+    if (Array.from(event.target.classList).includes('operator')) {
+        btnClickOperator(event.target.textContent);
+        return;
+    }
 }
 
 function btnClickNumber(number) {
-    if (Number(resultElement.textContent) === 0) {
+    if (Number(resultElement.textContent) === 0 || reset) {
         resultElement.textContent = number;
+        reset = false;
         return;
     }
     resultElement.textContent += number;
@@ -32,13 +45,50 @@ function btnClickDot() {
     }
 }
 
-function btnClickFunction(operator) {
-    console.log(operator);
-    switch (operator) {
+function btnClickFunction(fn) {
+    switch (fn) {
         case 'C':
-            console.log('hihi');
             resultElement.textContent = 0;
             break;
         default:
+    }
+}
+
+function btnClickOperator(op) {
+    btnClickOperatorByOperatorIsEqual(op);
+    operator = op;
+    reset = true;
+    resultElement.textContent = firstOperand;
+    console.log(`First Operand: ${firstOperand}\nOperator: ${operator}`);
+}
+
+function btnClickOperatorByOperatorIsEqual(op) {
+    if (op === '=') {
+        btnClickOperatorIfOperatorIsEqual();
+        return;
+    }
+    btnClickOperatorIfOperatorIsNotEqual();
+}
+
+function btnClickOperatorIfOperatorIsEqual(){
+    secondOperand = resultElement.textContent;
+    firstOperand = calculate(Number(firstOperand), operator, Number(secondOperand));
+    secondOperand = null;
+}
+
+function btnClickOperatorIfOperatorIsNotEqual(){
+    firstOperand = resultElement.textContent;
+}
+
+function calculate(firstOperand, operator, secondOperand) {
+    switch (operator) {
+        case '+':
+            return firstOperand + secondOperand;
+        case '-':
+            return firstOperand - secondOperand;
+        case '*':
+            return firstOperand * secondOperand;
+        case '/':
+            return firstOperand / secondOperand;
     }
 }
